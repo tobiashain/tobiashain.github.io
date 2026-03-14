6f99e8e0044db1194b82e4d8562cc7469575f55f
Tobinator647 <hain.tobias@outlook.at>
2026-03-14 21:43:31 +0100

    feat: bare bone experience section

--- src/Career.tsx
+++
@@ -1,7 +0,0 @@
-export default function Career() {
-  return (
-    <>
-      <div className="career"></div>
-    </>
-  );
-}
---
+++ src/career/Career.tsx
@@ -0,0 +1,78 @@
+import "./career.scss";
+export default function Career() {
+  return (
+    <>
+      <div className="career">
+        <p>Experience</p>
+        <div className="table">
+          <div className="line"></div>
+          <div className="row">
+            <div className="image">
+              <img src="https://picsum.photos/200" alt="" />
+            </div>
+            <div className="description">
+              <div className="date">Oct 2025 - Present</div>
+              <div className="activity">Example Co</div>
+              <div className="type">Full-Stack Developer</div>
+              <div className="context">
+                Web Design; Web Development; Wordpress + Elementor.
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="image">
+              <img src="https://picsum.photos/200" alt="" />
+            </div>
+            <div className="description">
+              <div className="date">Oct 2025 - Present</div>
+              <div className="activity">Example Co</div>
+              <div className="type">Full-Stack Developer</div>
+              <div className="context">
+                Web Design; Web Development; Wordpress + Elementor.
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="image">
+              <img src="https://picsum.photos/200" alt="" />
+            </div>
+            <div className="description">
+              <div className="date">Oct 2025 - Present</div>
+              <div className="activity">Example Co</div>
+              <div className="type">Full-Stack Developer</div>
+              <div className="context">
+                Web Design; Web Development; Wordpress + Elementor.
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="image">
+              <img src="https://picsum.photos/200" alt="" />
+            </div>
+            <div className="description">
+              <div className="date">Oct 2025 - Present</div>
+              <div className="activity">Example Co</div>
+              <div className="type">Full-Stack Developer</div>
+              <div className="context">
+                Web Design; Web Development; Wordpress + Elementor.
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="image">
+              <img src="https://picsum.photos/200" alt="" />
+            </div>
+            <div className="description">
+              <div className="date">Oct 2025 - Present</div>
+              <div className="activity">Example Co</div>
+              <div className="type">Full-Stack Developer</div>
+              <div className="context">
+                Web Design; Web Development; Wordpress + Elementor.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
